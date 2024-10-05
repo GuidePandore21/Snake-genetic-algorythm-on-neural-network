18a75d57e701e06d380eadcc2a9799974cfc17ee
GuidePandore21 <tlyautey1@gmail.com>
2024-10-05 15:26:26 +0200

    Feat : Implémentation de la méthode deplacement de la classe Network

--- NeuroneNetwork/Network.py
+++ NeuroneNetwork/Network.py
@@ -7,3 +7,15 @@ class Network:
         """
         self.layers = layers
         self.fitness = 0
+    
+    def deplacement(self, x, y, distance_sf):
+        """Mise à jour des valeurs des InputNeurones pour le déplacement du Snake
+
+        Args:
+            x (int): position en x du Snake
+            y (int): position en y du Snake
+            distance_sf (int): distance manhattan du Snake par rapport à la pomme
+        """
+        self.layers[0].neurones[0].inputData = x
+        self.layers[0].neurones[1].inputData = y
+        self.layers[0].neurones[4].inputData = distance_sf
