c4f2d6da2e9689f1d4d8083a15765d001930e875
GuidePandore21 <tlyautey1@gmail.com>
2024-10-05 15:20:36 +0200

    Feat : Implémentation de la méthode renameNeurones de la classe Layer

--- NeuroneNetwork/Layer.py
+++ NeuroneNetwork/Layer.py
@@ -7,3 +7,18 @@ class Layer:
         """
         self.label = label
         self.neurones = neurones
+    
+    def representation(self):
+        """retourne une liste contenant la liste des Neurone du Layer
+
+        Returns:
+            float: une liste contenant la liste des Neurone du Layer
+        """
+        layer = []
+        for neurone in self.neurones:
+            layer.append(neurone.representation())
+        return layer
+    
+    def renameNeurones(self):
+        for i in range(len(self.neurones)):
+            self.neurones[i].label = self.label + "Neurone" + str(i + 1)
