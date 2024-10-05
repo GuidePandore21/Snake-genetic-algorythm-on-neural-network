19ae4025f71185d9f9695a0bc31707470ed888db
GuidePandore21 <tlyautey1@gmail.com>
2024-10-05 15:42:04 +0200

    Feat : Initialisation du fichier de l'algorithme génétique

---
+++ AlgorithmeGenetique.py
@@ -0,0 +1,8 @@
+import random 
+import matplotlib.pyplot as plt
+from configAlgorithmeGenetique import *
+from NeuroneNetwork.Network import Network
+from NeuroneNetwork.Layer import Layer
+from NeuroneNetwork.Neurone import Neurone
+from NeuroneNetwork.InputNeurone import InputNeurone
+from NeuroneNetwork.OutputNeurone import OutputNeurone
