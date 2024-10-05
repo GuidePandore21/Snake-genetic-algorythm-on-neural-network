174ed98bf50baddfed651c01322b32a0947cd532
GuidePandore21 <tlyautey1@gmail.com>
2024-10-05 15:13:47 +0200

    Feat : implémentation de la méthode activation dans la classe Neurone

--- NeuroneNetwork/Neurone.py
+++ NeuroneNetwork/Neurone.py
@@ -1,3 +1,4 @@
+import math
 class Neurone:
     def __init__(self, label, bias, inputs) -> None:
         """Constructeur de la classe Neurone
@@ -31,3 +32,16 @@ class Neurone:
             calcul += input[0].output * input[1]
         self.output = self.activation(calcul)
         return self.output
+    
+    def activation(self, x):
+        """retourne la valeur de sortie de la fonction d'activation du Neurone,  sigmoid
+
+        Returns:
+            float: valeur de sortie de la fonction d'activation du Neurone
+        """
+        return max(0, x)
+        # return 1 / (1 + (math.exp(-x)))
+        
+
+# ∑wixi + biais = w1x1 + w2x2 + w3x3 + biais
+# sortie = f(x) = 1 si ∑w1x1 + b>= 0 ; 0 si ∑w1x1 + b < 0
