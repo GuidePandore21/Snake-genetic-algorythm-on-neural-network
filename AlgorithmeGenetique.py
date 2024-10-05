ee728e583141a011d23a68714fd43906267f1413
GuidePandore21 <tlyautey1@gmail.com>
2024-10-05 15:42:57 +0200

    Feat : Implémentation de la fonction triRapide

--- AlgorithmeGenetique.py
+++ AlgorithmeGenetique.py
@@ -6,3 +6,22 @@ from NeuroneNetwork.Layer import Layer
 from NeuroneNetwork.Neurone import Neurone
 from NeuroneNetwork.InputNeurone import InputNeurone
 from NeuroneNetwork.OutputNeurone import OutputNeurone
+
+# -------------------- OUTILS -------------------- #
+
+def triRapide(liste):
+    """tri une liste et la retourne
+
+    Args:
+        liste (Network): liste de Network
+
+    Returns:
+        liste (Network): liste de Network triée
+    """
+    if len(liste) <= 1:
+        return liste
+    else:
+        pivot = liste[0].fitness
+        infAPivot = [objet for objet in liste[1:] if objet.fitness <= pivot]
+        supAPivot = [objet for objet in liste[1:] if objet.fitness > pivot]
+        return triRapide(infAPivot) + [liste[0]] + triRapide(supAPivot)
