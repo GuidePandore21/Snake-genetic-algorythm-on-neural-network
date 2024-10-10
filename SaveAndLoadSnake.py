3493fee452886417e39cfc484bd3bc7c358115bc
Thomas Lyautey <tlyautey1@gmail.com>
2024-10-10 09:34:50 +0200

    Feat : Initialisation du fichier SaveAndLoad.py qui aura pour but de gérer les fonctions de sauvegarde et de chargement d'un individu

---
+++ SaveAndLoadSnake.py
@@ -0,0 +1,5 @@
+from NeuroneNetwork.Network import Network
+from NeuroneNetwork.Layer import Layer
+from NeuroneNetwork.Neurone import Neurone
+from NeuroneNetwork.InputNeurone import InputNeurone
+from NeuroneNetwork.OutputNeurone import OutputNeurone
