55aa3699a9e6c0b445f46433aa72ea38aa1965c1
Thomas Lyautey <tlyautey1@gmail.com>
2024-10-24 13:38:28 +0200

    Feat : Implémentation de la méthode changerValeurCase dans la classe Grille

--- Grille.py
+++ Grille.py
@@ -7,5 +7,15 @@ class Grille:
             height (int): hauteur de la fenêtre pygame en px (multiple de 10)
             width (int): largeur de la fenêtre pygame en px (multiple de 10)
         """
-        matrice = np.zeros((width // 10, height // 10), dtype=int)   
+        matrice = np.zeros((width // 10, height // 10), dtype=int) 
+        
+    def changerValeurCase(self, x, y, valeur):
+        """Change remplace la valeur de la case à la position x, y de la matrice par la valeur en paramètre
+
+        Args:
+            x (int): position en x dans la matrice
+            y (int): position en y dans la matrice
+            valeur (int): valeur dont va hériter la case, prends une de ces valeurs [-1, 0, 1, 2]
+        """
+        self.matrice[x, y] = valeur  
         
