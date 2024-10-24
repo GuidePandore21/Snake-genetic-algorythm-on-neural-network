44e6eb395b14fe11f94c06560d91844ecc73e55a
Thomas Lyautey <tlyautey1@gmail.com>
2024-10-24 13:31:30 +0200

    Docs : Ajout de la documentation pour le constructeur de la classe Grille

--- Grille.py
+++ Grille.py
@@ -1,5 +1,11 @@
 import numpy as np
 class Grille:
     def __init__(self, height, width):
-        matrice = np.zeros((height // 10, width // 10), dtype=int)
+        """Constructeur de la classe
+
+        Args:
+            height (int): hauteur de la fenêtre pygame en px (multiple de 10)
+            width (int): largeur de la fenêtre pygame en px (multiple de 10)
+        """
+        matrice = np.zeros((width // 10, height // 10), dtype=int)   
         
