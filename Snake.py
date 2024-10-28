6194c73d6370ba7f353ace4a83b543eaf829be23
Thomas Lyautey <tlyautey1@gmail.com>
2024-10-28 13:34:01 +0100

    Feat : Transfert des paramètres modifiable du fichier Snake.py dans le fichier configSnake.py pour faciliter les changements

--- Snake.py
+++ Snake.py
@@ -1,4 +1,3 @@
-import pygame
 import random
 import copy
 from configSnake import *
@@ -7,19 +6,6 @@ from snakeFonctions import *
 from Grille import Grille
 
 # -------------------- VARIABLES GLOBALES SNAKE -------------------- #
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-
-DIS_WIDTH = 300
-DIS_HEIGHT = 250
-DIS = pygame.display.set_mode((DIS_WIDTH, DIS_HEIGHT))
-CLOCK = pygame.time.Clock()
-
-SNAKE_BLOCK = 10
-SNAKE_SPEED = 10
 
 GRILLE = Grille(DIS_HEIGHT, DIS_WIDTH)
 INPUTS = GRILLE.matrice.flatten().tolist()
